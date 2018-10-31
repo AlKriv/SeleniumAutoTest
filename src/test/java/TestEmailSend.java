b0c838ed3dca850ba15b4ad8b345de8e68ebc04d
Alexandr Kr <testdert6705@gmail.com>
2018-10-31 15:18:43 +0200

    Final version 1

--- src/test/java/TestEmailSend.java
+++ src/test/java/TestEmailSend.java
@@ -3,11 +3,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.currentThread;
@@ -22,7 +27,10 @@ import static org.junit.Assert.assertEquals;
  */
 public class TestEmailSend
 {
+
+
     private WebDriver driver;
+    private  WebDriverWait wait;
     private String mail;
     private String pass;
     private String subj;
@@ -33,11 +41,11 @@ public class TestEmailSend
     public void Before()
     {
         driver=new ChromeDriver();
-        mail="SelForTest@gmail.com";
+        mail="selfortest@gmail.com";
         pass="10#@E(3oMU)y";
         subj="Test Selenium Web driver";
         bodyMail="Test Selenium Web driver Body";
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
         driver.get("https://mail.google.com/mail/?tab=wm");
     }
 
@@ -45,37 +53,49 @@ public class TestEmailSend
     public void Test()
     {
         //login
-        driver.findElement(By.cssSelector("input#identifierId")).sendKeys(mail);
+        WebElement fieldMail=(new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#identifierId")));
+        fieldMail.sendKeys(mail);
         driver.findElement(By.cssSelector("div#identifierNext")).click();
 
-      //  try {
-        //    Thread.sleep(5000);
-      //  } catch (InterruptedException e) {
-      //      e.printStackTrace();
-      //  }
         //password
-        driver.findElement(By.xpath(".//*[@id=\"password\"]/div/div/div/input")).sendKeys(pass);
+        WebElement fieldPass=(new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id=\"password\"]/div/div/div/input")));
+        fieldPass.sendKeys(pass);
         driver.findElement(By.cssSelector("div#passwordNext")).click();
-        //новое письмо
-        driver.findElement(By.cssSelector("div.T-I.J-J5-Ji.T-I-KE.L3")).click();
-        driver.findElement(By.cssSelector(".vO[name='to']")).sendKeys(mail);//кому
+
+        //new letter
+        WebElement newLetter=(new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.T-I.J-J5-Ji.T-I-KE.L3")));
+        newLetter.click();
+
+        //fill fields
+        WebElement fieldWhomSend=(new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(".vO[name='to']")));
+        fieldWhomSend.sendKeys(mail);
         driver.findElement(By.xpath("//input[@name='subjectbox']")).sendKeys(subj);//subject
-        driver.findElement(By.xpath("//div[@aria-label='Тело письма']")).sendKeys(bodyMail);//list
+        driver.findElement(By.xpath("//table[@id='undefined']//div[2]/div")).sendKeys(bodyMail);//list
         driver.findElement(By.xpath("//table[@role='group']/tbody/tr/td/div/div[2]")).click();//button send
         //check send
-        //driver.findElement(By.xpath("//div[@role='navigation']/div/div/div/div/div[4]/div/div/div[2]/span/a")).click();
-        driver.get("https://mail.google.com/mail/u/0/?tab=wm#sent");
-          try {
-            Thread.sleep(5000);
-          } catch (InterruptedException e) {
-              e.printStackTrace();
-          }
-        driver.findElement(By.xpath("//div[@role='main']//tbody/tr")).click();
-          //subject
+
+
+        //click send button
+        WebElement sendButton= (new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='navigation']/div/div/div/div/div[4]/div")));
+        sendButton.click();
+
+        try {
+            sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement letterFieldSummary=(new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='main']//tbody/tr")));
+        letterFieldSummary.click();
+
+
+        //wait for load page
+        //get data field
+        //subject
         String tempSubj=(driver.findElement(By.xpath("//table[@role='presentation']//h2"))).getText();
         //mail
-        String tempMail =(driver.findElement(By.xpath("//span[@role='gridcell']/span[2]"))).getText();
-        //body letter
+         String tempMail =(driver.findElement(By.xpath("//*[@role='listitem']//span/span"))).getAttribute("email");
+       //body letter
         String tempLetter = (driver.findElement(By.xpath("//div[@role='gridcell']/div"))).getText();
 
 
@@ -84,15 +104,16 @@ public class TestEmailSend
 
 
     }
+
+
+
     @After
     public void After()
     {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         driver.findElement(By.xpath("//*[@id=\":5\"]/div[3]/div[1]/div/div[2]/div[3]/div")).click();
-//  //*[@id=":5"]/div[3]/div[1]/div/div[2]/div[3]/div/div
+
     }
+
+
 }
